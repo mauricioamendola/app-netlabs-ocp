1a85d3d958058eeacfc74435e0043a72fce17120
Mauricio <mauricio@linux.com.uy>
2020-11-24 14:23:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get('/headers', function (req, res) {
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 8080!');
 });
 
 function enable_app(seconds) {
